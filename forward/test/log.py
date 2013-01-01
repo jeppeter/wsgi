d6c93aed5710a6f29a2eca7624683a79d8632ddd
jeppeter <jeppeter@gmail.com>
2013-01-01 17:46:00 +0800

    to modify log.py

--- forward/test/log.py
+++ forward/test/log.py
@@ -1,5 +1,28 @@
 #! python
 
 import logging
+import sys
+from optparse import OptionParser
+
+def SetLevel(option,opt,value,parser):
+    if value == "debug":
+    	setattr(parser.values,option.dest,loggin.DEBUG)
+    elif value == "warning" || value == "warn":
+    	setattr(parser.values,option.dest,loggin.WARNING)
+    elif value == "critical":
+    	setattr(parser.values,option.dest,loggin.CRITICAL)
+    elif value == "info":
+    	setattr(parser.values,option.dest,loggin.INFO)
+    else:
+    	sys.stderr.write("level not valid (%s)\n"%(str(value)))
+    	sys.exit(3)
+
+optarg = OptionParser()
+optarg.add_option("--level","-l",dest="Level",default=loggin.ERROR,action="callback",callback=SetLevel,help="specify the debug level")
+optarg.add_option("--format","-f",dest="Format",help="specify the format")
+
+optarg.parse_args()
+
+
 
 logging.info()
