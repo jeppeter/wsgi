f026cdf0c049328c57b03f118619718a744c8452
jeppeter <jeppeter@gmail.com>
2013-01-01 11:36:59 +0800

    to modify the testzlib.py as the zlib.compressobj and zlib.decompressobj

--- forward/test/testzlib.py
+++ forward/test/testzlib.py
@@ -3,28 +3,49 @@
 import zlib
 import sys
 
+def CmprOut(data):
+	sys.stdout.write(data)
+
+def DeCmprOut(data):
+	#sys.stdout.write(data)
+	pass
+
 def Compress():
-    todata = ''
-    for data in sys.stdin:
-		todata += data
-		sys.stderr.write("Read "+data)
-    data = zlib.compress(todata,zlib.Z_BEST_COMPRESSION)
-    sys.stderr.write("Compress %d\n"%(len(data)))
-    sys.stdout.write(data)
+	count = 0
+	rcount = 0
+	cmprobj=zlib.compressobj(zlib.Z_BEST_COMPRESSION)
+	for data in sys.stdin:
+		cmprdata = cmprobj.compress(data)
+		#sys.stderr.write("Read "+data)
+		rcount += len(data)
+		if cmprdata :
+			count += len(cmprdata)
+			CmprOut(cmprdata)
+	cmprdata = cmprobj.flush()
+	CmprOut(cmprdata)
+	count += len(cmprdata)
+	sys.stderr.write("Compress %d read %d\n"%(count,rcount))
     
 
 def DeCompress():
     todata = ''
     count = 0
+    dcount = 0
+    decmprobj = zlib.decompressobj()
     while True:
 		data = sys.stdin.read(1024)
 		count += len(data)
-		sys.stderr.write("read %d\n"%(count))
 		if len(data) ==0:
 			break
-		todata += data
-    sys.stderr.write("De Read %d "%(len(todata)))
-    data =zlib.decompress(todata)
+		dedata = decmprobj.decompress(data)
+		if dedata:
+			dcount += len(dedata)
+			DeCmprOut(dedata)
+    dedata = decmprobj.flush()
+    dcount += len(dedata)
+    DeCmprOut(dedata)
+    sys.stderr.write("De Read %d out %d\n"%(count ,dcount))
+    
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
