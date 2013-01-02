6192d0ccda96b2f67011f57b9d5989937e1e7647
jeppeter <jeppeter@gmail.com>
2013-01-02 09:34:17 +0800

    make forward.py handle environment print out

--- forward/local/forward.py
+++ forward/local/forward.py
@@ -1,10 +1,18 @@
 #! python
 
 import sys
-import signal
 import gevent
-from gevent.server import StreamServer
-from gevent.socket import create_connection, gethostbyname
+from gevent.pywsgi import WSGIServer
+import logging
+
+def DebugEnv(env):
+	res = ''
+	for k,v in env.items():
+		curres = "evn[%s] = %s"%(k,v)
+		logging.info(curres)
+		curres += "\n"
+		res += curres
+	return res
 
 """
 	@brief Proxy to handle the proxy 
@@ -18,7 +26,23 @@ from gevent.socket import create_connection, gethostbyname
 
 	 @return value content of the response
 """
-def Proxy(env,start_response):
-	
+def proxy_application(env,start_response):
+	resbody = DebugEnv(env)
+	status = '200 OK'
+	response_headers = [('Content-Type', 'text/plain'),
+                  ('Content-Length', str(len(resbody)))]
+	start_response(status, response_headers)
+	return [resbody]
 
-class ForwardServer(StreamServer):
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		sys.stderr.write("%s hostip:port\n"%(__file__))
+		sys.exit(3)
+	logging.basicConfig(level=logging.INFO,format="%(levelname)-8s %(asctime)-12s [%(filename)-10s:%(funcName)-20s:%(lineno)-5s] %(message)s")
+	arg = sys.argv[1]
+	listenip,listenport=arg.split(":")
+	listenport = int(listenport)
+	logging.info("listen on %s:%d"%(listenip,listenport))
+	WSGIServer((listenip,listenport), proxy_application).serve_forever()
