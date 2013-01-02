28e8f3b9076604b2c4c37b10fea3cd2e7fd07a9b
jeppeter <jeppeter@gmail.com>
2013-01-02 08:55:47 +0800

    to add the doxygen doc in the forward.py

--- forward/local/forward.py
+++ forward/local/forward.py
@@ -6,5 +6,19 @@ import gevent
 from gevent.server import StreamServer
 from gevent.socket import create_connection, gethostbyname
 
+"""
+	@brief Proxy to handle the proxy 
+	      in the form like this
+	      Enc-Method:
+	      Enc-Url:
+	  	  ....
+
+	 @param env environment dict
+	 @param start_response callback function to send response
+
+	 @return value content of the response
+"""
+def Proxy(env,start_response):
+	
 
 class ForwardServer(StreamServer):
