297f2af616519afdbc35a8c749b9e698569bb328
jeppeter <jeppeter@gmail.com>
2013-01-03 20:41:00 +0800

    to add the sslcli.py remote read

--- forward/test/sslcli.py
+++ forward/test/sslcli.py
@@ -7,6 +7,16 @@ import traceback
 # patches stdlib (including socket and ssl modules) to cooperate with other greenlets
 monkey.patch_all()
 
+def CliRemoteRead(sslsock,gsock):	
+	while True:
+		try:
+			data = sslsock.recv(1024)
+			if not data or len(data)==0 :
+				break
+			gsock.send(data)
+		except:
+			break
+	return 0
 
 def CliSSL(gsock,paddr):
 	try:
@@ -15,6 +25,16 @@ def CliSSL(gsock,paddr):
 	except:
 		gsock.shutdown()
 		return -1
-
+	chld = gevent.spawn(CliRemoteRead,(rsslsock,gsock,))
 	while True:
-		
+		try:
+			data = gsock.recv(1024)
+			if not data or len(data) == 0 :
+				break
+			rsslsock.send(data)
+		except:
+			break
+	gevent.join(chld)
+	return 0
+
+
